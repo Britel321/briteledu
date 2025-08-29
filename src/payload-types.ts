4752368d110d849da6cfe4797e2fc31b38f0097b
sanjayguwaju <sanjayguwaju@gmail.com>
2025-08-29 20:43:31 +0545

    feat: add ESLint configuration for TypeScript and Next.js; update payload-types to use generic types for improved flexibility in BasicSliderSelect interface

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -2406,20 +2406,20 @@ export interface UniversitiesBlockSelect<T extends boolean = true> {
  * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "Basic Slider_select".
  */
-export interface BasicSliderSelect {
-  richText?: boolean;
+export interface BasicSliderSelect<T extends boolean = true> {
+  richText?: T;
   slides?:
-    | boolean
+    | T
     | {
-        image?: boolean;
-        title?: boolean;
-        description?: boolean;
-        alt?: boolean;
-        id?: boolean;
+        image?: T;
+        title?: T;
+        description?: T;
+        alt?: T;
+        id?: T;
       };
-  autoplaySpeed?: boolean;
-  id?: boolean;
-  blockName?: boolean;
+  autoplaySpeed?: T;
+  id?: T;
+  blockName?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
